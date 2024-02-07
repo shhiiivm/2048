8ae5e547456d7b8ecd74540386f7bedbfedbd4d2
shhiiivm <yadavshivam2020s@gmail.com>
2024-02-08 02:34:49 +0530

    main

---
+++ Assets/Scripts/TileState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Tile State")]
+public class TileState : ScriptableObject
+{
+
+    public Color backgroundColor;
+
+    public Color textColor;
+}
